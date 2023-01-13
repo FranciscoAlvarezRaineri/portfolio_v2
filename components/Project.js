2c1869de24b892d8c6b20bd5707a2cb1667d006f
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-01-13 11:51:38 -0300

    change readme

---
+++ components/Project.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Project = ({ project }) => {
+  const { title, subTitle, content, funcs, imgs, imgAlt, tools, live, repo } =
+    project;
+  return (
+    <div className="flex items-center justify-items-center">
+      <div className="md:basis-1/3 basis-1/2 m-4 aspect-video">
+        <a href={live ? live : repo} target="blank">
+          <img src={imgs} alt={imgAlt}></img>
+        </a>
+      </div>
+      <div className="md:basis-2/3 basis-1/2 flex content-between md:flex-row flex-col">
+        <div className="basis-1/2 m-4">
+          <h1 className="text-2xl leading-loose">{title}</h1>
+          <h1 className="text-xl leading-loose">{subTitle}</h1>
+          <p className="text-lg leading-loose">{content}</p>
+          <ul className="list-disc ml-4">
+            {funcs?.map((func, i) => (
+              <li key={`tool_${i}`} className=" text-lg">
+                {func}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="basis-1/2 m-4">
+          <h1 className="md:text-center text-2xl leading-loose">
+            What I've Learned:
+          </h1>
+          <ul className="list-disc">
+            {tools?.map((tool, i) => (
+              <li
+                key={`tool_${i}`}
+                className="md:text-center text-lg leading-loose"
+              >
+                {tool}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
---
+++ pages/api/contact.js
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+require("dotenv").config();
+const PASSWORD = process.env.password;
+
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: "smtp.gmail.com",
+  auth: {
+    user: "FranciscoAlvarezRaineri@gmail.com",
+    pass: PASSWORD,
+  },
+  secure: true,
+});
+
+export default function handler(req, res) {
+  const mailData = {
+    from: "FranciscoAlvarezRaineri@gmail.com",
+    to: "FranciscoAlvarezRaineri@gmail.com",
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) res.status(404).send(err);
+    else {
+      res
+        .status(200)
+        .send(
+          "The e-mail was sent correctly. I'll answer shortly. Thank you for getting in touch!"
+        );
+    }
+  });
+}
